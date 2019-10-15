337ac192a8ed5d7de21a8abcbaedb384d59ed9cc
谢博4 <xiebo4@xdf.cn>
2019-10-15 17:50:52 +0800

    first commit

---
+++ src/main/java/space/xbgt/photowallserver/controller/GetFileList.java
@@ -0,0 +1,52 @@
+package space.xbgt.photowallserver.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.lang.System;
+import java.io.File;
+import java.util.ArrayList;
+/**
+ * controller
+ *
+ * @author xiebo
+ **/
+@RestController
+public class GetFileList {
+    
+    @Value("${space.xbgt.author}")
+    private String author;
+
+    @Value("${space.xbgt.photoDirectory}")
+    private String photoDirectory;
+
+    private ArrayList<String> filelist = new ArrayList<String>();
+
+    void getFiles(String filePath) {
+        File root = new File(filePath);
+        File[] files = root.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                getFiles(file.getAbsolutePath());
+            } else {
+                String filename;
+                filename = file.getAbsolutePath().replace("\\","/").replace(photoDirectory, "");
+                System.out.println("filename "+filename);
+                filelist.add(filename);
+            }
+        }
+    }
+
+    @RequestMapping("/GetFileList")
+    public String method() {
+        getFiles(photoDirectory);
+        System.out.println("getfilelist received");
+        return "haha";
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
